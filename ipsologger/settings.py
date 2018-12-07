854c886c0957d21a12dafcc651ed11f683cc86f9
Boussard <yboussard@numericube.com>
2018-12-07 11:08:45 +0100

    pep8

--- ipsologger/settings.py
+++ ipsologger/settings.py
@@ -1,4 +1,6 @@
+"""Define a custom logconfig for gunicorn and django."""
 
+# Define our default fields.
 default_fields = (
         '@timestamp',
         '@hostname',
@@ -20,126 +22,107 @@ default_fields = (
         'threadName',
         'levelname')
 
-
+# Format fields.
 format_str = ' '.join([
         '%({})s'.format(el)
         for el
         in default_fields
     ])
 
+# Date format.
 DATEFORMAT = '%Y-%m-%dT%H:%M:%S,%03d'
 
+# Define logger.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
         'default': {
-            'format': '%(message)s'
-                    },
+            'format': '%(message)s'},
         'json-gunicorn': {
             'format': format_str,
             'datefmt': DATEFORMAT,
             'tag': 'gunicorn',
-            '()': 'ipsologger.formatter.JsonFormatter',
-                    },
+            '()': 'ipsologger.formatter.JsonFormatter',},
         'json-gunicorn-access': {
             'datefmt': DATEFORMAT,
             'tag': 'gunicorn_access',
             'format': format_str + '%(access)',
-            '()': 'ipsologger.formatter.JsonFormatter',
-         },
-        
+            '()': 'ipsologger.formatter.JsonFormatter',},
         'json-python': {
             'format': format_str,
             'datefmt': DATEFORMAT,
             'tag': 'python',
-            '()': 'ipsologger.formatter.JsonFormatter',
-                    },
-        
+            '()': 'ipsologger.formatter.JsonFormatter',},
         'json-django': {
             'format': format_str,
             'datefmt': DATEFORMAT,
             'tag': 'django',
-            '()': 'ipsologger.formatter.JsonFormatter',
-        },
+            '()': 'ipsologger.formatter.JsonFormatter',},
     },
     'filters': {
         'default': {
-            '()': 'jslog4kube.KubeMetaInject',
-                    },
-            },
+            '()': 'jslog4kube.KubeMetaInject',},
+    },
     'handlers': {
         'json-stdout': {
             'level':'DEBUG',
             'class':'logging.StreamHandler',
             'stream': 'ext://sys.stdout',
             'formatter': 'json-python',
-            'filters': ['default'],
-                    },
+            'filters': ['default']},
         'json-gunicorn': {
             'level':'DEBUG',
             'class':'logging.StreamHandler',
             'stream': 'ext://sys.stdout',
             'formatter': 'json-gunicorn',
-            'filters': ['default'],
-                    },
+            'filters': ['default']},
         'json-gunicorn-access': {
             'level':'DEBUG',
             'class':'logging.StreamHandler',
             'stream': 'ext://sys.stdout',
             'formatter': 'json-gunicorn-access',
-            'filters': ['default'],
-                    },
-        
+            'filters': ['default']},
         'json-django': {
             'level':'DEBUG',
             'class':'logging.StreamHandler',
             'stream': 'ext://sys.stdout',
             'formatter': 'json-django',
-            'filters': ['default'],
-                    },
-        
-        
+            'filters': ['default']},
         'default': {
             'level':'DEBUG',
             'class':'logging.StreamHandler',
             'stream': 'ext://sys.stdout',
             'formatter': 'default',
-            'filters': ['default'],
-                    },
-            },
+            'filters': ['default']},
+     },
     'loggers': {
         'django': {
             'handlers': ['json-django',],
             'propagate': True,
             'level': 'INFO',
             'filters': ['default'],
-            'formatters': ['json'],
-                    },
+            'formatters': ['json']},
         'gunicorn': {
             'handlers': ['json-gunicorn'],
             'formatters': ['json'],
             'propagate': False,
-            'level':'ERROR',
-                    },
+            'level':'ERROR'},
         'gunicorn.access': {
             'handlers': ['json-gunicorn-access'],
             'formatters': ['json-access'],
             'propagate': False,
-            'level':'INFO',
-                    },
+            'level':'INFO'},
         'gunicorn.error': {
             'handlers': ['json-gunicorn'],
             'formatters': ['json'],
             'propagate': False,
-            'level':'INFO',
-                    },
+            'level':'INFO'},
         'requests': {
             'handlers': ['json-stdout',],
             'formatters': ['json'],
             'propagate': True,
             'filters': ['default'],
-            'level':'INFO',
-                    },
-            }
+            'level':'INFO'}
     }
+}
